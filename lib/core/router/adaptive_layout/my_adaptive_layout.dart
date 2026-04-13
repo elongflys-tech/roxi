647089d478e19be7069478887e4a5a33f8c7074d
elongflys-tech <elongflys@gmail.com>
2026-04-14 01:05:28 +0800

    feat: unified node list with Shadowrocket-style swipe UI (v110)

--- lib/core/router/adaptive_layout/my_adaptive_layout.dart
+++ lib/core/router/adaptive_layout/my_adaptive_layout.dart
@@ -93,7 +93,6 @@ class MyAdaptiveLayout extends HookConsumerWidget {
 
   List<ShellRouteAction> _actions(Translations t, bool showProfilesAction, bool isMobileBreakpoint) => [
     ShellRouteAction(Icons.power_settings_new_rounded, t.pages.home.title),
-    if (showProfilesAction && !isMobileBreakpoint) ShellRouteAction(Icons.public_rounded, '节点'),
     ShellRouteAction(Icons.settings_rounded, t.pages.settings.title),
     if (!isMobileBreakpoint) ShellRouteAction(Icons.description_rounded, t.pages.logs.title),
     if (!isMobileBreakpoint) ShellRouteAction(Icons.info_rounded, t.pages.about.title),
